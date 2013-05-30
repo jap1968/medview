e9b3e7f3149146402bd2ee0b58a50495e9dd108a
jap1968 <josean1968@gmail.com>
2013-05-30 09:49:50 +0200

    main application files: first release

---
+++ js/medview.js
@@ -0,0 +1,66 @@
+/**
+ * Main medview script
+ * jQuery required
+ */
+
+/**
+ * @namespace Main medview namespace.
+ */ 
+var medview = medview || {};
+
+// Alternative method to load .js files
+/*
+var medviewBase = "js/";
+$.getScript(medviewBase + "html.js",
+  function(data, textStatus, jqxhr) {
+    // console.log(data); //data returned
+    console.log(textStatus); //success
+    console.log(jqxhr.status); //200
+    medviewInit();
+  });
+*/
+var mainDG;
+
+function medviewInit() {
+  console.log("medview loaded");
+  
+  // ToDo: Join these initialization steps
+  var dg = new medview.gateway.DicomGateway();
+  mainDG = dg;
+  var fg = new medview.html.CFindForm($('#controls'), dg);  
+  dg.studyList = new medview.html.StudyList($('#cFindArea'));
+  dg.seriesList = new medview.html.SeriesList($('#cFindArea'));
+
+  var adj = new medview.html.AdjustForm($('#adjControls'), dg);
+ 
+//  id="imgGrid";
+}
+
+$(document).ready(function(){
+    // initialise the application
+    medviewInit();
+    medviewResize();
+});
+
+// Avoid multiple resize invocations
+// http://stackoverflow.com/a/668185/176974
+
+function medviewResize() {
+
+  console.log("resize");
+  var viewportWidth = $(window).width();
+  var viewportHeight = $(window).height();
+  // alert("Size: W: " + viewportWidth + ", H: " + viewportHeight);
+  mainDG.setSize(viewportWidth, viewportHeight);
+//  console.log($(this));
+
+}
+
+var resizeTimeOut = false;
+$(window).resize(function(){
+  if (resizeTimeOut !== false) {
+    clearTimeout(resizeTimeOut);
+  }
+  resizeTimeOut = setTimeout(medviewResize, 200);
+});
+
